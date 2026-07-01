f9a58a059972c945ab0c600f17f95305e4872810
nguyen842-ai <nguyen842@gmail.com>
2026-07-01 08:58:55 +0700

    Update dbActive.js with new entries and status changes

--- dbActive.js
+++ dbActive.js
@@ -1,9 +1,14 @@
 // dbActive.js
 
 const dbActive = [
-   {hq:"1.South", ar: "1. HCMC", k: "DSR Lương Thành Phát",ac: "✔️"},
+   {hq:"1.South", ar: "1. HCMC", k: "DSR Lương Thành Phát",ac: "❌"},
 {hq:"1.South", ar: "1. HCMC", k: "KAE Lâm Thị Cẩm Linh",ac: "✔️"},
 {hq:"1.South", ar: "1. HCMC", k: "KAE Nguyễn Thị Sao Mai Anh",ac: "✔️"},
 {hq:"1.South", ar: "1. HCMC", k: "DSR Nguyễn Tuyết Trinh",ac: "❌"},
+{hq:"2.North", ar: "1. Ha Noi", k: "KAE Phạm Sỹ Hùng",ac: "✔️"},
+{hq:"2.North", ar: "1. Ha Noi", k: "KAE Vũ Quang Phúc",ac: "✔️"},
+{hq:"1.South", ar: "2. North Mekong", k: "DSR Nguyễn Thanh Tân",ac: "✔️"},
+{hq:"1.South", ar: "2. North Mekong", k: "DSR Huỳnh Văn Khánh",ac: "✔️"},
+
 
 ];
