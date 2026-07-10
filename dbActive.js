afe60015948447809c1fc85c0d92bc8702efa750
nguyen842-ai <nguyen842@gmail.com>
2026-07-10 18:12:20 +0700

    Delete specific entries from dbActive.js
    Removed entries for DSR Nguyễn Thanh Tân and DSR Huỳnh Văn Khánh from the database.

--- dbActive.js
+++ dbActive.js
@@ -7,8 +7,5 @@ const dbActive = [
 {hq:"1.South", ar: "1. HCMC", k: "DSR Nguyễn Tuyết Trinh",ac: "❌"},
 {hq:"2.North", ar: "1. Ha Noi", k: "KAE Phạm Sỹ Hùng",ac: "✔️"},
 {hq:"2.North", ar: "1. Ha Noi", k: "KAE Vũ Quang Phúc",ac: "✔️"},
-{hq:"1.South", ar: "2. North Mekong", k: "DSR Nguyễn Thanh Tân",ac: "✔️"},
-{hq:"1.South", ar: "2. North Mekong", k: "DSR Huỳnh Văn Khánh",ac: "✔️"},
-
 
 ];
